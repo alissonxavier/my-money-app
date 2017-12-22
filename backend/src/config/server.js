0a56a9e4f7248324ef99bd04631b2143a72eee80
Alisson Xavier de Albuquerque <alisson.xavier@ifactory.com.br>
2017-12-21 21:47:18 -0300

    Inclusão dos react e redux

---
+++ backend/src/config/server.js
@@ -0,0 +1,19 @@
+const port = 3033
+
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
+
+
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser())
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = server
+
